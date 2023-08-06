e4594c212207717c6512a7e356e269c1d6e3c593
tanmen <yt.prog@gmail.com>
2023-08-07 02:07:56 +0900

    add step1 sample

---
+++ src/samples/step1.js
@@ -0,0 +1,10 @@
+const data = [1,2,3,4,5,6,7,8,9,10]
+
+console.log('forEach')
+data.forEach((datum) => console.log(datum))
+
+
+console.log('for')
+for (const datum of data) {
+    console.log(datum)
+}
