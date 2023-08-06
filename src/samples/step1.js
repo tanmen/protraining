8d9a829c2c3e2bbb2722f649b198197c4f8a4ddd
tanmen <yt.prog@gmail.com>
2023-08-07 02:19:29 +0900

    add function

--- src/samples/step1.js
+++ src/samples/step1.js
@@ -2,6 +2,9 @@ const data = [1,2,3,4,5,6,7,8,9,10]
 
 console.log('forEach')
 data.forEach((datum) => console.log(datum))
+data.forEach(function (datum) {
+    console.log(datum);
+})
 
 
 console.log('for')
